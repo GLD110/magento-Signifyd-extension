b169a85eb0a7e678fbed2e1efcabd3e35125fe2f
diana4545 <diana4545@outlook.com>
2016-03-31 05:12:24 +0300

    Backup
    copy and paste registration.php

---
+++ registration - Copy.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © 2015 SIGNIFYD Inc. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Signifyd_Connect',
+    __DIR__
+);
